52e521efaab10e29ab655542880b221646663003
Joao Brito <joaobrito@MBP-de-Joao.lan>
2023-07-28 10:42:24 +0100

    Initial commit with first test case and bare bones of framework for UI testing

---
+++ pages/HomePage.js
@@ -0,0 +1,58 @@
+const { expect } = require('@playwright/test');
+
+exports.HomePage = class Homepage {
+
+  /**
+   * @param {import('@playwright/test').Page} page
+   */
+  constructor(page)
+    {this.page = page;
+    this.getLoginLink = page.getByRole('link', { name: 'Log in' });
+    this.getUsernameField = page.locator('#loginusername');
+    this.getPasswordField = page.locator('#loginpassword');
+    this.getLoginButton = page.getByRole('button', { name: 'Log in' });
+    this.getAddToCartButton = page.getByRole('link', { name: 'Add to cart' });
+    }
+
+    //Homepage
+    async loginUser()
+    { await this.getLoginLink.click();
+    await this.getUsernameField.click();
+    await this.getUsernameField.fill(process.env.USERNAME);
+    await this.getUsernameField.press('Tab');
+    await this.getPasswordField.fill(process.env.PASSWORD);
+    await this.getLoginButton.click()
+    }
+
+    async selectProductByText(product)
+    { await this.page.getByRole('link', { name: `${product}`}).click();
+    }
+    
+    //Top Tabs Navigation
+    async navigateToArea(area)
+    {   await this.page.sleep(3000);
+        await this.page.getByRole('link', { name: `${area}`}).click();
+    }
+
+    //Alerts
+    async closeAlert()
+    {   this.page.once('dialog', dialog => {
+        console.log(`Dialog message: ${dialog.message()}`);
+        dialog.dismiss().catch(() => {});
+      });
+    }   
+    
+    async addToCart()
+    {   await this.getAddToCartButton.click();  }
+
+    async tapNextButton()
+    {   await this.page.locator('#next2').click();  }
+
+    async isUserLoggedIn()
+    {   if((await this.page.$('#login2')) !== null){
+            this.loginUser();}
+    ;}
+
+};
+
+
---
+++ tests/new_test.spec.js
@@ -0,0 +1,48 @@
+const { HomePage } = require('../pages/Homepage.js');
+import { test, expect, page } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  const homepage = new HomePage(page);
+
+  await test.step('Log in', async () => {
+    await page.goto('https://demoblaze.com/')
+    await homepage.loginUser();
+  });;
+
+  await test.step('Add Iphone to Cart', async () => {
+    await homepage.selectProductByText('Iphone 6 32gb');
+    await homepage.addToCart();
+    });;
+
+  await test.step('Add Laptop to Cart', async () => {
+    await homepage.navigateToArea('Home (current)');
+    await homepage.isUserLoggedIn(); //Check if user is logged in
+    await homepage.navigateToArea('Laptops');  
+    await homepage.tapNextButton();
+    await homepage.selectProductByText('ASUS Full HD');
+    await homepage.addToCart();
+    });;
+
+  await test.step('Verify Items added to Cart', async () => {
+  //TODO Asserts
+    await page.getByRole('cell', { name: 'ASUS Full HD' }).click();
+    await page.getByRole('cell', { name: 'Iphone 6 32gb' }).click();
+    });;
+
+  
+  await page.getByRole('link', { name: 'Cart', exact: true }).click();
+  
+
+  await page.getByRole('button', { name: 'Place Order' }).click();
+  await page.getByLabel('Total: 1020').click();
+  
+  //Finish Order
+  await page.getByLabel('Total: 1020').fill('Joao');
+  await page.getByLabel('Total: 1020').press('Tab');
+  await page.getByLabel('Country:').press('Tab');
+  await page.getByLabel('City:').press('Tab');
+  await page.getByLabel('Credit card:').fill('111111111');
+  await page.getByRole('button', { name: 'Purchase' }).click();
+  await page.getByText('Id: 2194939Amount: 1020 USDCard Number: 111111111Name: JoaoDate: 24/6/2023').click();
+  await page.getByRole('button', { name: 'OK' }).click();
+});
